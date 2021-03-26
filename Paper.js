ba04a0e42bbb36edb60eab4afe2fa795a717eacb
mshaswat2004 <78262057+mshaswat2004@users.noreply.github.com>
2021-03-26 13:19:58 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,27 @@
+class Paper {
+    constructor(x, y, width, height){
+        var options = {
+            isStactic : false,
+            restitution : 0.3,
+            friction : 0.5,
+            density : 1.2
+        }
+
+      this.body = Bodies.rectangle(x, y, 20, height, options);
+      this.width = 20;
+      this.height = height;
+      this.image = loadImage("sprites/paper.png") ;
+      Matter.Body.setAngle(this.body, angle);
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+     imageMode(CENTER);
+      image(this.image,0, 0, this.width, this.height);
+      pop();
+    }
+  } 
